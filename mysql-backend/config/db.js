57baa9f1faa15841c88ed6c7aefa818e87d58c22
Krishnanjaly <krishnanjalys98@gmail.com>
2026-03-25 00:19:44 +0530

    Updated the dburl

--- mysql-backend/config/db.js
+++ mysql-backend/config/db.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
+// Create connection pool using either a single URL or individual variables
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false,
   },
   waitForConnections: true,
   connectionLimit: 10,
-  connectTimeout: 10000, // 🔥 ADD THIS
+  connectTimeout: 20000,
 });
 
 module.exports = pool.promise();
